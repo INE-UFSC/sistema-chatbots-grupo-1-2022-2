4aa8d0b6b1597ca310a5a755490fdcbb60c8d624
William Kraus <krwilliam03@gmail.com>
2022-09-30 10:54:21 -0300

    implementar Bot.py

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -5,29 +5,58 @@ import random as r
 
 class Bot(ABC):
 
-    def __init__(nome,):
-        self.nome = nome
-        self.comandos = {}
+    def __init__(self, nome,):
+        self.__nome = nome
+        self.__comandos = {}
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+
+    @property
+    def comandos(self):
+        return self.__comandos
+    
+
+    def dizer(self, frase: str):
+        return f"--> {self.__nome} diz: {frase}"
 
     def mostra_comandos(self):
+        return "\n".join(
+            f"{i+1} - {comando}" for i, comando in enumerate(self.comandos)
+        )
+
+    def executa_comando(self, cmd: str):
+        comandos = list(self.comandos.items())
+
+        pergunta_resposta = comandos.get(int(cmd))
+
+        if pergunta_resposta is None:
+            return "\n".join((
+                self.dizer("Eu não sei o que você disse."),
+                self.mostra_comandos()
+            ))
+        
+        pergunta, resposta = pergunta_resposta
+        
+        return "\n".join((
+            f"--> Você disse: {pergunta}",
+            self.dizer(resposta)
+        ))
+            
+
+    @abstractmethod
+    def apresentacao(self):
         pass
 
     @abstractmethod
-    def executa_comando(self,cmd):
-        pass
-
-    @abstractmethod
-    def boas_vindas():
+    def boas_vindas(self):
         pass
     
     @abstractmethod
-    def despedida():
+    def despedida(self):
         pass
