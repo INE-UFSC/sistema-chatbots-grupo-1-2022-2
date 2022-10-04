84d94a6933dbe8b5049f0249d74eac0ad9c05a31
enzoBrum <darosabrumenzo@gmail.com>
2022-10-04 11:27:19 -0300

    add bot grupo6

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ from Bots.BotZangado import BotZangado
 from Bots.BotInteligente import BotInteligente
 from Bots.Grupo3.BotFeliz import BotFeliz
 from Bots.Grupo5.BotTriste import BotTriste
+from Bots.Grupo6.BotGrupo6 import BotGrupo6
 
 ###construa a lista de bots disponíveis aqui
 lista_bots = [
     BotZangado("Yoda"),
     BotInteligente("Salomão"),
     BotFeliz("FELIZ"),
-    BotTriste()
+    BotTriste(),
+    BotGrupo6("1")
+    
 ]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
