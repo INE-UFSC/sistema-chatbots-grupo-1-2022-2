527b73ca22f37aae76d36eef3b58117fe2b1c285
enzoBrum <darosabrumenzo@gmail.com>
2022-10-04 10:52:12 -0300

    add bots from group3 to app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 from Bots.BotInteligente import BotInteligente
+from Bots.Grupo3.BotFeliz import BotFeliz
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotInteligente("Salomão")]
+lista_bots = [BotZangado("Yoda"), BotInteligente("Salomão"), BotFeliz("FELIZ")]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
