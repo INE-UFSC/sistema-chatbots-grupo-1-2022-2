f4f8a18e57fabf8980fdc1602a2408758966eabc
William Kraus <krwilliam03@gmail.com>
2022-10-04 11:16:32 -0300

    adicionar BotTriste do grupo 5

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 from Bots.BotInteligente import BotInteligente
 from Bots.Grupo3.BotFeliz import BotFeliz
+from Bots.Grupo5.BotTriste import BotTriste
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotInteligente("Salomão"), BotFeliz("FELIZ")]
+lista_bots = [
+    BotZangado("Yoda"),
+    BotInteligente("Salomão"),
+    BotFeliz("FELIZ"),
+    BotTriste()
+]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
